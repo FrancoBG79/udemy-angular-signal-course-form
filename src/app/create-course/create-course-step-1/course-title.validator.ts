6650c0c09af11eaf66975961a478073fed65b835
FrancoBG79 <fbg2468@gmail.com>
2026-07-14 09:50:59 +0000

    section 4 completed

--- src/app/create-course/create-course-step-1/course-title.validator.ts
+++ src/app/create-course/create-course-step-1/course-title.validator.ts
@@ -1,6 +1,41 @@
-import { SchemaPath, validateHttp } from '@angular/forms/signals';
+import { SchemaPath, validateAsync } from '@angular/forms/signals';
 import { CourseListResponse } from './step1.model';
+import { resource } from '@angular/core';
+// import { CourseListResponse } from './step1.model';
 
-export function courseTitleExists(path: SchemaPath<string>) {
-
+// export function courseTitleExists(path: SchemaPath<string>) {
+//   validateHttp<string, CourseListResponse>(path, {
+//     request: (ctx) => (ctx.value() ? '/api/courses' : undefined),
+//     onSuccess: (result, ctx) => {
+//       const found = result?.payload?.find(
+//         (c) => c.description.toLocaleLowerCase() === ctx.value()?.toLocaleLowerCase()
+//       );
+//       return found ?
+//         { kind: 'titleExists', message: 'This title is already being used.' } : null;
+//     },
+//     onError: () => null,
+//   })
+// }
+export function courseTitleExistsAsync(path: SchemaPath<string>) {
+  validateAsync<string, string, CourseListResponse>(path, {
+    params: (ctx) => ctx.value() ?? '',
+    factory: (params) => resource({
+      params,
+      loader: async({ params: title}) => {
+        if (!title) return undefined;
+        const res = await fetch('/api/courses');
+        return await res.json() as CourseListResponse;
+      }
+    }),
+    onSuccess: (result, ctx) => {
+      const found = result?.payload?.find((c) => 
+          c.description.toLocaleLowerCase() === ctx.value()?.toLocaleLowerCase()
+        );
+      return found ? { 
+        kind: 'titleExists',
+        message: 'This title is already being used.',
+      } :  null;
+    },
+    onError: () => null,
+  })
 }
